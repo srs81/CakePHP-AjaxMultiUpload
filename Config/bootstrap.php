958d056b1ca2d4e358fd41f602e5b57f7435fd19
Suman <suman.srinivasan@gmail.com>
2012-03-12 18:27:06 -0400

    Added GPL/MIT license info

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,4 +1,15 @@
 <?php
-	Configure::write ('AMU.directory', 'files');
+/**
+ *
+ * Dual-licensed under the GNU GPL v3 and the MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2012, Suman (srs81 @ GitHub)
+ * @package       plugin
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ *                and/or GNU GPL v3 (http://www.gnu.org/copyleft/gpl.html)
+ */
+
+ 	Configure::write ('AMU.directory', 'files');
 	Configure::write ('AMU.filesizeMB', '2');
 ?>
--- Controller/AjaxMultiUploadAppController.php
+++ Controller/AjaxMultiUploadAppController.php
@@ -1,5 +1,15 @@
 <?php
-
+/**
+ *
+ * Dual-licensed under the GNU GPL v3 and the MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2012, Suman (srs81 @ GitHub)
+ * @package       plugin
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ *                and/or GNU GPL v3 (http://www.gnu.org/copyleft/gpl.html)
+ */
+ 
 require_once (APP . "Plugin/AjaxMultiUpload/Vendor/valums/upload.php");
 
 class AjaxMultiUploadAppController extends AppController {
